f473c9ac96dc012511e15e52e5a22f1f187e696a
Debra Fabbri <debrafabbri@gmail.com>
2022-06-02 23:54:51 -0300

    Dark-mode

--- script.js
+++ script.js
@@ -35,3 +35,30 @@ btns.forEach((btn) => {
     });
 });   
 
+
+//---------------------------------------------------------------------------------------------------------------------
+//Modo Nocturno
+
+const btnSwitch = document.querySelector('#switch');
+
+btnSwitch.addEventListener('click', () => {
+    document.body.classList.toggle('dark');
+    btnSwitch.classList.toggle('active');
+
+    //Guardamos el Modo Nocturno en el localStorage
+    if(document.body.classList.contains('dark')){
+        localStorage.setItem('dark-mode', 'true');
+    }else{
+        localStorage.setItem('dark-mode', 'false');
+    }
+
+});
+
+//Obtenemos el modo actual donde nos encontramos
+if(localStorage.getItem('dark-mode') === 'true'){
+    document.body.classList.add('dark');
+    btnSwitch.classList.add('active');
+}else{
+    document.body.classList.remove('dark');
+    btnSwitch.classList.remove('active');
+}
